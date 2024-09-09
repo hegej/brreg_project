5fd9f6a46701a35f339a33b14c46afc72520864e
hegej <hege.jacobsen12@gmail.com>
2024-09-09 13:26:08 +0200

    Added Models to brreg_api

--- brreg_api/models.py
+++ brreg_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.model):
+    org_number = models.CharField(max_length=9, unique=True)
+    name = models.CharField(max_length=255)
+    is_bankrupt = models.BooleanField(default=False)
+    is_as = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} ({self.org_number})"
